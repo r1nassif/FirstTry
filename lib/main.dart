d1050106c4f27d0e7b63d679bd76bb4896f33d1f
razan1 <Razan.nasif@hotmail.com>
2023-02-27 20:49:48 +0300

    Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+      
+      backgroundColor: Colors.amber,
+       ),)
+       ),
+       ),
+       );
+}
+
+
+
+
